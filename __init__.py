070eeabf730aee20a4029fe5386bf6153f60fcc9
scudre <20219711+scudre@users.noreply.github.com>
2021-09-07 19:06:01 -0700

    committed changes

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@
 # XXXXX
 # https://community.home-assistant.io/t/howto-use-temperature-from-weather-home-as-trigger/200260
 
-
+import signal
 import time
 from datetime import date
 from datetime import datetime
@@ -24,24 +24,31 @@ from messageprovider import MessageProvider
 CLOCK_ENABLED = False
 CLOCK_FONT = proportional(TINY_FONT)
 MSG_FONT = proportional(TINY_FONT)
-LONG_MSG_LEN = 11
+
+# Replace with nice state class?
+VALID_DISPLAY_VALUES = [
+    'blank'
+    'forecast',
+    'current',
+    'notifications'
+    ]
 
 weather = {
 'sunny'     : 0x9142183dbc184289,
 'foggy'     : 0x55aa55aa55aa55aa,
-'cloudy'    : 0x00007e818999710e,
+'cloudy'    : 0x00007c8282621c00,
 'rainy'     : 0x152a7e818191710e,
 'snowy'     : 0xa542a51818a542a5,
 'tstorm'    : 0x0a04087e8191710e,
 'windy'     : 0x005f807f001f2010,
-'clear'     : '',
+'clear'     : 0x000c26034313460c,
 }
 
 ha_weather_map = {
     'clear-night'     : 'clear', 
     'cloudy'          : 'cloudy',
     'fog'             : 'foggy',
-    'hail'            : 'tstorm'
+    'hail'            : 'tstorm',
     'lightning'       : 'tstorm',
     'lightning-rainy' : 'tstorm',
     'partlycloudy'    : 'cloudy',
@@ -55,6 +62,35 @@ ha_weather_map = {
     'exceptional'     : 'sunny',
 }    
 
+class Display(object):
+    def __init__(self, serial):
+        self.device = max7219(serial, cascaded=4, block_orientation=-90, blocks_arranged_in_reverse_order=False)
+        self.device.contrast(0)
+        self._state = 'blank'
+        #signal.signal(signal.SIGINT, self.exit_gracefully)
+        #signal.signal(signal.SIGTERM, self.exit_gracefully)
+        
+    def state(self):
+        return self._state
+        
+    def blank(self):
+        return self._state == 'blank'
+        
+    def forecast(self):
+        return self._state == 'forecast'
+        
+    def current(self):
+        return self._state == 'current'
+    
+    def notifications(self):
+        return self._state == 'notifications'
+
+    
+
+def weather_icon(name):
+    mapping = ha_weather_map.get(name, 'sunny')
+    return weather.get(mapping)
+
 def icon(draw, origin, icon_hex):
     points = []
     for y in range(8):
@@ -63,37 +99,32 @@ def icon(draw, origin, icon_hex):
 
     draw.point(points, fill="white")
 
-class HoursMinutes:
-    def __init__(self):
-        self.ts = datetime.now()
-        self._set_hm()
-
-    def next(self):
-        self.ts = self.ts + timedelta(seconds=1)
-        self._set_hm()
-    
-    def _set_hm(self):
-        self.hours = self.ts.strftime('%H')
-        self.minutes = self.ts.strftime('%M')
-
 
 def cp437_encode(str):
    return [c.encode('cp437') for c in str]
 
 
-def animation(device, from_y, to_y):
+def animation(device, weather, from_y, to_y):
     """Animate the whole thing, moving it into/out of the abyss."""
-    ts = now()
     current_y = from_y
     while current_y != to_y:
-        draw_clock(device, ts, y_val=current_y)
+        show_curr_weather(device, weather, current_y)
         time.sleep(0.065)
         current_y += 1 if to_y > from_y else -1
 
-def vertical_scroll(device, words):
-    ts = now()
+
+def transition(device, weather, from_y, to_y, display_func):
+    """Animate the whole thing, moving it into/out of the abyss."""
+    current_y = from_y
+    while current_y != to_y:
+        display_func(device, weather, current_y)
+        time.sleep(0.065)
+        current_y += 1 if to_y > from_y else -1
+
+def vertical_scroll(device, words=["foo", "bar", "bat", "bang"]):
     messages = [" "] + words + [" "]
     virtual = viewport(device, width=device.width, height=len(messages) * 12)
+    import pdb; pdb.set_trace()
     
     first_y_index = 0
     last_y_index = (len(messages) - 1) * 12
@@ -112,87 +143,89 @@ def vertical_scroll(device, words):
             time.sleep(10)
         time.sleep(0.044)
 
-def draw_clock_2(draw, ts, y_val=1, toggle=True):
-    text(draw, (0, y_val), ts.hours, fill="white", font=CLOCK_FONT)
-    text(draw, (15, y_val), ":" if toggle else " ", fill="white", font=proportional(TINY_FONT))
-    text(draw, (17, y_val), ts.minutes, fill="white", font=CLOCK_FONT)
-    #text(draw, (device.width - 2 * 8, 1), day_of_week(), fill="white", font=proportional(CLOCK_FONT))
 
-
-def draw_clock(device, ts, y_val=1, toggle=True):
+def show_curr_weather(device, weather, y_val=0):
     with canvas(device) as draw:
-        text(draw, (0, y_val), ts.hours, fill="white", font=CLOCK_FONT)
-        text(draw, (15, y_val), ":" if toggle else " ", fill="white", font=proportional(TINY_FONT))
-        text(draw, (17, y_val), ts.minutes, fill="white", font=CLOCK_FONT)
-        #text(draw, (device.width - 2 * 8, 1), day_of_week(), fill="white", font=proportional(CLOCK_FONT))
+        house = 0x00003e2a22140800
+        icon(draw, (-1, y_val), house)
+        text(draw, (6, y_val), weather.get('in_temp', '?'), fill="white", font=proportional(TINY_FONT))
+        icon(draw, (15, y_val), weather_icon(weather.get('cond')))
+        text(draw, (24, y_val), weather.get('out_temp', '?'), fill="white", font=proportional(TINY_FONT))
+
+def draw_forecast(device, weather, y_val=0):
+    with canvas(device) as draw:
+        icon(draw, (0, y_val), weather_icon(weather.get('for_cond')))
+        text(draw, (11, y_val), weather.get('for_temp', '?'), fill="white", font=proportional(TINY_FONT))
+
+def show_forecast(device, weather, y_val=0):
+    # XXX fix up animations -- use virtual canvas
+    animation(device, weather, 0, 8)
+    transition(device, weather, -8, 0, draw_forecast)
+
+    draw_forecast(device, weather)
+
+    time.sleep(40)
+    transition(device, weather, 0, -8, draw_forecast)
+
+    animation(device, weather, 8, 0)
+    show_curr_weather(device, weather)
+
+
+def show_notifications(device, msg_provider):
+    #animation(device, 1, 8)
+    for msg in msg_provider.messages(filter_topics=['weather']):
+        show_message(device, cp437_encode(full_msg), fill="white", font=proportional(CLOCK_FONT), scroll_delay=0.022)
+    #animation(device, 8, 1)
+    time.sleep(1)
 
 def main():
     serial = spi(port=0, device=0, gpio=noop())
-    # XXX make configurable
-    device = max7219(serial, cascaded=4, block_orientation=90, blocks_arranged_in_reverse_order=True)
-    device.contrast(0)
-    
-    msg = str(chr(177) * 8)
-    #show_message(device, msg, fill="white", font=CP437_FONT)
+    display = Display(serial)
     
     # Start mqtt message subscription
     msg_provider = MessageProvider(config)
     msg_provider.loop_start()
     
-    # The time ascends from the abyss...
-    if CLOCK_ENABLED:
-        animation(device, 8, 1)
+    #vertical_scroll(device)
+
+    def exit_gracefully(signal, frame):
+        print('stopping')
+        msg_provider.loop_stop()
+        exit(0)
+    signal.signal(signal.SIGINT, exit_gracefully)
+    signal.signal(signal.SIGTERM, exit_gracefully)
     
-    toggle = False  # Toggle the second indicator every second
-    count = 0
+    print('okay lets go')
     while True:
-        try:
-            toggle = not toggle
-            sec = datetime.now().second
-            if sec == 59 and CLOCK_ENABLED:
-                # When we change minutes, animate the minute change
-                minute_change(device)
-            elif sec == 10:
-                today = date.today()
-                #messages = [today.strftime("%2d.%2m.%4Y")] +
-                #messages = [m for m in msg_provider.messages() if len(m) <= LONG_MSG_LEN]
-                #print(msg_provider.messages())
-                #vertical_scroll(device, messages)
-            elif sec == 40:
-                print('here')
-                count = count+1
-                today = date.today()
-                long_messages = [ m for m in msg_provider.messages() if len(m) > LONG_MSG_LEN ]
-                print(msg_provider.messages())
-                if count == 10 and len(long_messages) > 0:
-                    count = 0
-                    messages = long_messages
-                    #animation(device, 1, 8)
-                    for full_msg in messages:
-                       show_message(device, cp437_encode(full_msg), fill="white", font=proportional(CLOCK_FONT), scroll_delay=0.022)
-                    #animation(device, 8, 1)
-                    time.sleep(1)
+        curr_time = datetime.now()
+        #show_notifications(device, msg_provider)
+        #after showing notifications, default to whatever curr screen should
+        # show
+         
+        # update weather
+        # can this be done keeping the old position
+        # after updating weather -- based on time see if we need
+        # to move position to show weather or forecast
+        
+        if curr_time.minute % 5 == 0:
+            # XXX fix up animations -- use virtual canvas
+            weather = msg_provider.message('weather')
+            print('show forecast')
+            print(weather)
+            show_forecast(display.device, weather)
+        else:
+            print('current')
+        
+            weather = msg_provider.message('weather')
+            print(weather)
+            #check for notifications to display
+            # pull from queue display twice?
+            show_curr_weather(display.device, weather)
+            
+            display.current = True
+            
+        time.sleep(30)
 
-            elif CLOCK_ENABLED:
-                #device.clear()
-                
-                ts = now()
-                draw_clock(device, ts, toggle=toggle)
-
-                time.sleep(0.5)
-            else:
-                message = (msg_provider.messages() or [''])[0]
-                with canvas(device) as draw:
-
-                    val =  #weather['STORM'] #0x00003e2a22140800 #0x18187e247e241800 #0xff99998142241800
-                    icon(draw, (0, 0), val)
-                    #text(draw, (8, 0), '68', fill="white", font=proportional(TINY_FONT))
-       
-                    text(draw, (10, 0), message, fill="white", font=proportional(TINY_FONT))
-                
-        except KeyboardInterrupt:
-            msg_provider.loop_stop()
-            break
 
 
 if __name__ == "__main__":
